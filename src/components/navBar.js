4f0226963f403465f5c6e4126aeecab254638ba5
Vryahn <b.ra@live.com.mx>
2022-12-20 16:48:52 -0600

    background white, fixing buttons and flex

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,49 +1,79 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
-
-function ColorSchemesExample() {
+function NavScrollExample() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark" className='d-flex'>
-        <Container>
-          <Navbar.Brand href="#home">NavBar</Navbar.Brand>
+    <Navbar bg="white" expand="lg">
+      <Container fluid>
+        <div className="d-flex">
+          <div>
+            <a className="navbar-brand" href="./index.html">
+              <img
+                className="div__nav-menu img__devto-logo"
+                src="https://dev-to-uploads.s3.amazonaws.com/uploads/logos/resized_logo_UQww2soKuUsjaOGNB38o.png"
+                alt="dev logo"
+                width="50"
+                height="40"
+              />
+            </a>
+          </div>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              role="img"
+              aria-labelledby="aqzwa9cip0tswtyys6etth18jpas5abq"
+              class="crayons-icon"
+            >
+              <title id="aqzwa9cip0tswtyys6etth18jpas5abq">Search</title>
+              <path d="M18.031 16.617l4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0111 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 01-1.969 5.617zm-2.006-.742A6.977 6.977 0 0018 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 004.875-1.975l.15-.15z"></path>
+            </svg>
+          </Form>
+        </div>
+        <div>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "300px" }}
+              navbarScroll
+            >
+              {/* Ejemplo de menú desplegable */}
+              {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown> */}
 
-          {/* //Starts form */}
-          {/* <Form>
-          <Row className="align-items-start">
-          <Col sm={7} className="my-1">
-          <Form.Control placeholder="Search" />
-        </Col>
-        </Row>
-        </Form> */}
-
-        <InputGroup className="align-items-start">
-        <Form.Control
-          placeholder="Search"
-          aria-label="Search"
-          aria-describedby="basic-addon2"
-        />
-        <Button variant="outline-secondary" className="d-none d-sm-none d-md-none d-lg-block btn btn-search" id="button-addon2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true" class="crayons-icon c-btn__icon" focusable="false"><path d="m18.031 16.617 4.283 4.282-1.415 1.415-4.282-4.283A8.96 8.96 0 0 1 11 20c-4.968 0-9-4.032-9-9s4.032-9 9-9 9 4.032 9 9a8.96 8.96 0 0 1-1.969 5.617zm-2.006-.742A6.977 6.977 0 0 0 18 11c0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7a6.977 6.977 0 0 0 4.875-1.975l.15-.15z"></path></svg>
-        </Button>
-      </InputGroup>
-
-          <Nav className="me-3">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    
-    </>
+              <Nav.Link href="#" disabled className="text-black">
+                Log in
+              </Nav.Link>
+              <Button variant="outline-primary" height="40">
+                <p>Create account</p>
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Container>
+    </Navbar>
   );
 }
 
-export default ColorSchemesExample;
+export default NavScrollExample;
