367a8fbfd6a20abe81d08ebab71c8951b1f3e65f
sueenciso <susana.enciso@icloud.com>
2022-12-20 14:35:58 -0600

    change color in navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-
-import NavBar from './components/navBar';
-import BasicExample from './components/navBar';
-import ColorSchemesExample from './components/navBar';
+import NavScrollExample from './components/navBar';
 function App() {
   return (
     <div className="App">
-      <ColorSchemesExample/>
+      <NavScrollExample/>
     </div>
   );
 }
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,23 +1,51 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function ColorSchemesExample() {
+function NavScrollExample() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <Navbar bg="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#" className='text-white'>Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1" className='text-white'>Home</Nav.Link>
+            <Nav.Link href="#action2" className='text-white'>Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled className='text-white'>
+              Link
+            </Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
-    
-    </>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default ColorSchemesExample;
+export default NavScrollExample;
